806914fa1bc77b3398942ef0e4144841d92f0146
nico <ndeleon@itba.edu.ar>
2019-12-26 01:26:30 -0300

    Little tweaks in eventos model and erase code in postrelations

--- eventos/models.py
+++ eventos/models.py
@@ -4,7 +4,6 @@ from django.utils.text import slugify
 from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 
-
 user = settings.AUTH_USER_MODEL
 
 STATUS_EVENT ={
@@ -13,13 +12,13 @@ STATUS_EVENT ={
     ('F','Finished'),
     ('R','Refused')
 }
+
 STATUS_EVENTPOST ={
     ('2BO','To_be_open'),
     ('O','Open'),
     ('C','Close'),
     ('F','Finished')
 }
-
 
 class fotos(models.Model):
     updloader = models.ForeignKey(user, default = 1, blank = True, on_delete=models.CASCADE)
@@ -33,13 +32,13 @@ class eventpost(models.Model):
     users = models.ManyToManyField(user,blank=True,verbose_name="list of users",related_name="+")
     createTime = models.DateTimeField(auto_now = True)
     posts = models.ManyToManyField(fotos, blank = True, verbose_name="publicaciones")
-    code = models.CharField(max_length = 5,null=True)
+    code = models.CharField(max_length = 5,null=True,unique=True)
     users_winners = models.ManyToManyField(user,blank=True,verbose_name="list of users Winners",related_name="+")
-    status =  models.CharField(choices = STATUS_EVENTPOST, default = "O", max_length=3)
+    status =  models.CharField(choices = STATUS_EVENTPOST, default = "2BO", max_length=3)
     stock = models.IntegerField(default = 0)
     scoring = models.IntegerField(default = 0)
     stockW = models.IntegerField(default = 0)
-    #Overrides Call of object by its title
+
     def __str__(self):
         return self.title
 
@@ -49,7 +48,6 @@ class postrelations(models.Model):
     createTime = models.DateTimeField(auto_now = True)
     winer_code = models.CharField(max_length=20,verbose_name="Code to Retrieve")
     status = models.CharField(choices = STATUS_EVENT, default = "2BA", max_length=3)
-    code = models.CharField(max_length = 5,null=True)
 
     class Meta:
         ordering = ['-createTime']
@@ -62,17 +60,9 @@ class postrelations(models.Model):
 
     def get_delete_url(self):
         return f"delete/{self.slug}"       
-    
-    #Overrides Call of object by its EVENT_TITLE - INSTAACCOUNT
+
     def __str__(self):
         return (self.event.title + ' - ' + self.person.instaaccount)
 
     def instaaccount(self):
         return self.person.instaaccount
-
-    #override save method to define a unique slug
-   # def save(self, *args, **kwargs):
-   #     super(BarPost, self).save(*args, **kwargs)
-   #     if not self.slug:
-   #         self.slug = slugify(self.company + "-" + str(self.id))
-   #         self.save()
