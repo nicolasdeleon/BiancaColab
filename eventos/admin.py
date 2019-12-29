929b174e6d8f04ed7eefe47951b9e4a96221bbc7
damian-bit <51974962+damian-bit@users.noreply.github.com>
2019-12-29 18:45:33 -0300

    Update admin.py

--- eventos/admin.py
+++ eventos/admin.py
@@ -4,9 +4,41 @@ from django.contrib import admin
 
 from .models import eventpost, fotos, postrelations
 
+from django.contrib import messages
+'''DEBUG	debug
+INFO	info
+SUCCESS	success
+WARNING	warning
+ERROR	error'''
 
 def status_winner(modeladmin, request, queryset):
-	queryset.update(status='W')
+		for q in queryset:
+				if q.status == "2BA":
+						event = q.event
+						if event.stock << event.stockW and event.status == "O":
+						 event.stockW +=1
+						 event.save()
+						 q.status='W'
+						 q.save()
+						 messages.success(request, 'Se marcó como winner')
+						 if event.stock == event.stockW:
+						 	event.status = "C"
+						 	event.save()
+						else:
+						 #event.status == "C"
+						 messages.error(request, 'Evento concluido')
+				else:
+					messages.error(request, 'Relación ya marcada como ganadora')
+
+		   
+		     #evento = eventpost.objects.get(person = pr.event )
+		     
+
+
+
+	#evento.stockW +=1
+
+	#queryset.update(status='W')
 
 def status_refused(modeladmin, request, queryset):
 	queryset.update(status='R')
