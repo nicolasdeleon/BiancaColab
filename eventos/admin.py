31713bd63461028c1979c26a059443ebd51a6baa
damian-bit <51974962+damian-bit@users.noreply.github.com>
2020-01-06 21:51:06 -0300

    set_status_finished a postrelations

--- eventos/admin.py
+++ eventos/admin.py
@@ -11,7 +11,7 @@ SUCCESS	success
 WARNING	warning
 ERROR	error'''
 
-def status_winner(modeladmin, request, queryset):
+def set_status_winner(modeladmin, request, queryset):
 		for q in queryset:
 				if q.status == "2BA":
 						event = q.event
@@ -30,24 +30,26 @@ def status_winner(modeladmin, request, queryset):
 				else:
 					messages.error(request, 'Relación ya marcada como ganadora')
 
-		   
-		     #evento = eventpost.objects.get(person = pr.event )
-		     
 
-
-
-	#evento.stockW +=1
-
-	#queryset.update(status='W')
-
-def status_refused(modeladmin, request, queryset):
+def set_status_refused(modeladmin, request, queryset):
 	queryset.update(status='R')
+
+def set_status_finished(modeladmin, request, queryset):
+		for q in queryset:
+				if q.status == "W":
+						event = q.event
+						q.status='F'
+						q.save()
+						messages.success(request, 'Se marcó como pagada')
+				else:
+					messages.error(request, 'Relación no ganadora')
+					
 
 
 class postrelationsadmin(admin.ModelAdmin):
 	list_filter = ('status',)
 	list_display = ('event','instaaccount','status','createTime',)
-	actions = [status_winner, status_refused]
+	actions = [set_status_winner, set_status_refused, set_status_finished]
 
 
 
