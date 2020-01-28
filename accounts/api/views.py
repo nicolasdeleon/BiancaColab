7f76eead8cd0c3053a510849d382617c7ef78761
nico <ndeleon@itba.edu.ar>
2020-01-28 11:25:48 -0300

    Bump password reset email

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -250,78 +250,7 @@ def send_feedback_view(request):
 		smtp.sendmail(EMAIL_ADDRESS,EMAIL_ADDRESS,msg)
 	return Response(context)
 
-#Obtengo las credenciales ya verificadas del mail configurado para mandar mails
-EMAIL_ADDRESS2 = 'flororsi@gmail.com' #ACA HAY Q PONER LA CUENTA DE SUPPORT DE BIANCA Y DARLE LOS PERMISOS CORRESPONDIENTES
-EMAIL_PASSWORD2 = 'ozdktmppasgklser'
-'''
-@api_view(['POST', ])
-@permission_classes((IsAuthenticated, ))
-def reset_password(request):
-	context = {}
-	#CHECK QUE EFECTIVAMENTE ESTA MI USER
-	try:
-		user = request.user
-	except User.DoesNotExist:
-		context['response'] = 'Error'
-		context['error_message'] = 'User does not exist'
-		return Response(context,status=status.HTTP_404_NOT_FOUND)
-	
-	user.reset_password_token = binascii.hexlify(os.urandom(10)).decode()[0:10]
-	user.save()
 
-	#Tengo mi user
-	with smtplib.SMTP('smtp.gmail.com',587) as smtp:
-		smtp.ehlo()
-		smtp.starttls()
-		smtp.ehlo()
-		
-		smtp.login(EMAIL_ADDRESS2,EMAIL_PASSWORD2)
-
-		subject = 'Password Reset'
-		body = f'{user.full_name} tu clave de reseteo es: {user.reset_password_token}. \nPor favor colocarla en la aplicacion para su cambiar su password. \n\nSi usted no solicito el cambio de password por favor desestime el email.'
-		msg = f'Subject: {subject}\n\n{body}'
-		
-		context['response'] = "Success"
-
-		smtp.sendmail(user.email,user.email,msg)
-	return Response(context)
-
-
-
-class reset_password_confirm(UpdateAPIView):
-
-	serializer_class = ChangePasswordSerializer
-	model = user
-	permission_classes = (IsAuthenticated,)
-	authentication_classes = (TokenAuthentication,)
-
-	def get_object(self, queryset=None):
-		obj = self.request.user
-		return obj
-
-	def update(self, request, *args, **kwargs):
-		self.object = self.get_object()
-		serializer = self.get_serializer(data=request.data)
-
-		if serializer.is_valid():
-			# Check old password
-			if not self.object.reset_password_token == serializer.data.get("old_password"):
-				return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-
-			# confirm the new passwords match
-			new_password = serializer.data.get("new_password")
-			confirm_new_password = serializer.data.get("confirm_new_password")
-			if new_password != confirm_new_password:
-				return Response({"new_password": ["New passwords must match"]}, status=status.HTTP_400_BAD_REQUEST)
-
-			# set_password also hashes the password that the user will get
-			self.object.set_password(serializer.data.get("new_password"))
-			self.object.save()
-			return Response({"response":"successfully changed password"}, status=status.HTTP_200_OK)
-
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-'''
 @api_view(['POST', ])
 @permission_classes((AllowAny, ))
 def reset_password(request):
@@ -329,24 +258,22 @@ def reset_password(request):
 	email = request.data.get('email')
 	try:
 		user_aux = user.objects.get(email = email)
-		#data["codeMail"] = "OK"
 		user_aux.reset_password_token = binascii.hexlify(os.urandom(6)).decode()[0:6]
 		user_aux.save()
-		#Tengo mi user
-		with smtplib.SMTP('smtp-relay.gmail.com',587) as smtp:
-			smtp.ehlo()
-			smtp.starttls()
-			smtp.ehlo()
-			
-			smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+		
+		subject = "Password Reset"
+		context = {
+			"reset_token": user_aux.reset_password_token
+		}
+		message = render_to_string("reset_password.html", context)
 
-			subject = 'Password Reset'
-			body = f'{user_aux.full_name} tu clave de reseteo es:\n\n{user_aux.reset_password_token}\n\nIngresala en la app junto con la nueva password.\nSi no solicitaste el cambio de password desestimar el mail.'
-			msg = f'Subject: {subject}\n\n{body}'
-			
-			data['response'] = "Success"
+		from_email = settings.SUPPORT_EMAIL
 
-			smtp.sendmail(user_aux.email,user_aux.email,msg)
+		mail = EmailMultiAlternatives(subject, from_email, [user_aux.email], body='')
+		mail.attach_alternative(html_body, "text/html")
+		mail.send()
+
+		data['response'] = "Success"
 		return Response(data=data)
 	except user.DoesNotExist:
 		data['response'] = 'Error'
