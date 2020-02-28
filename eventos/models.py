c558d515e9e2e6cf99ec18b117aabaf77126ee03
nico <ndeleon@itba.edu.ar>
2020-02-27 22:01:47 -0300

    Integrated DropBox and some minor model changes

--- eventos/models.py
+++ eventos/models.py
@@ -19,8 +19,12 @@ STATUS_EVENTPOST = [
 ]
 
 class  InstaStoryPublication(models.Model):
-    updloader = models.ForeignKey(user, default=1, blank=True, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='image/', blank=True, null=True)
+    person = models.ForeignKey(user, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='Events_Users_Storys/', blank=True, null=True)
+
+    def __str__(self):
+        return "Instagram story from @" + str(self.person.instaaccount)
+
 
 class eventpost(models.Model):
     title = models.CharField(max_length=30)
@@ -42,11 +46,12 @@ class eventpost(models.Model):
 
 class postrelations(models.Model):
     person = models.ForeignKey(user, default=1, blank=True, on_delete=models.CASCADE)
-    event = models.ForeignKey(eventpost, default=1, blank=True, on_delete=models.CASCADE)
+    event = models.ForeignKey(eventpost, default=1, on_delete=models.CASCADE)
     createTime = models.DateTimeField(auto_now=True)
     winer_code = models.CharField(max_length=20, verbose_name="Code to Retrieve")
     status = models.CharField(choices=STATUS_EVENT, default="2BA", max_length=3)
     notificationToken = models.CharField(max_length=255, blank=True, null=True)
+    story = models.ForeignKey(InstaStoryPublication, blank=True, null=True, on_delete=models.DO_NOTHING)
 
     class Meta:
         ordering = ['-createTime']
