cc6746bae8dea431929988a5b51186f12787d40b
nico <ndeleon@itba.edu.ar>
2020-04-16 01:39:53 -0300

    Fix admin eventos logic

--- eventos/admin.py
+++ eventos/admin.py
@@ -8,58 +8,18 @@ from .models import Event, InstaStoryPublication, Post
 
 
 def send_push_message(token, message, extra=None):
-    # logger = logging.getLogger(__name__)
-    # logger.error("Test!!")
     try:
         response = PushClient().publish(
             PushMessage(to=token,
                         body=message))
     except PushServerError as exc:
-        # Encountered some likely formatting/validation error.
-        # rollbar.report_exc_info(9_1
-        #     extra_data={
-        #         'token': token,
-        #         'message': message,
-        #         'extra': extra,
-        #         'errors': exc.errors,
-        #         'response_data': exc.response_data,
-        #     })
         raise
-"""
-    except (ConnectionError, HTTPError) as exc:
-        # Encountered some Connection or HTTP error - retry a few times in
-        # case it is transient.
-        # rollbar.report_exc_info(9_!
-        #     extra_data={'token': token, 'message': message, 'extra': extra})
-        raise self.retry(exc=exc)
-
-    try:
-        # We got a response back, but we don't know whether it's an error yet.
-        # This call raises errors so we can handle them with normal exception
-        # flows.
-        response.validate_response()
-    except DeviceNotRegisteredError:
-        # Mark the push token as inactive
-        from notifications.models import PushToken
-        PushToken.objects.filter(token=token).update(active=False)
-    except PushResponseError as exc:
-        # Encountered some other per-notification error.
-        rollbar.report_exc_info(
-            extra_data={
-                'token': token,
-                'message': message,
-                'extra': extra,
-                'push_response': exc.push_response._asdict(),
-            })
-        raise self.retry(exc=exc)
-"""
 
 def set_status_winner(modeladmin, request, queryset):
     for q in queryset:
         if q.status == "2BA":
             event = q.event
-            if event.stock << event.activeParticipants and event.status == "O":
-                
+            if event.stock > event.activeParticipants and event.status == "O":
                 event.save()
                 q.status = 'W'
                 var_token = q.notificationToken
@@ -69,9 +29,8 @@ def set_status_winner(modeladmin, request, queryset):
                 try:
                     send_push_message(token=var_token, message='Aprobado! Ahora busca tu beneficio!')
                 except:
-                    return  
+                    return
             else:
-                # event.status == "C"
                 messages.error(request, 'Evento concluido')
         else:
             messages.error(request, 'Relación ya finalizada')
@@ -94,7 +53,7 @@ def set_status_finished(modeladmin, request, queryset):
 
 class PostAdmin(admin.ModelAdmin):
     list_filter = ('status',)
-    list_display = ('event', 'instaaccount', 'status', 'createTime')
+    list_display = ('event', 'instaAccount', 'status', 'createTime')
     actions = [set_status_winner, set_status_refused, set_status_finished]
 
 
