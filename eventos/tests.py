e677a3b82c0dc0350b6ee87fb76cf95f99759c9d
damian-bit <51974962+damian-bit@users.noreply.github.com>
2020-05-07 22:37:16 -0300

    Update with pylint

--- eventos/tests.py
+++ eventos/tests.py
@@ -9,7 +9,6 @@ from eventos.admin import set_status_winner
 # Create your tests here.
 
 class ApiEvenCreateTests(APITestCase):
-        
     def setUp(self):
         self.credentials = {
             "email" : "userforevent@test.com",
@@ -21,67 +20,50 @@ class ApiEvenCreateTests(APITestCase):
             first_name="oliver",
             last_name="twist",
             role="1"
-
-
-        )
-        
+            )
         # THIS SHOULD BE CHANGED IF WE ARE USING CONFIRMATION EMAIL FOR USERS
         self.email_confirmed, email_is_created = EmailConfirmed.objects.get_or_create(user=self.user)
         self.email_confirmed.confirmed = True
         self.email_confirmed.save()
         self.token = Token.objects.get(user=self.user)
-        self.profile = Profile(user=self.user,instaAccount= "insta").save()
-   
-        print("test setup")
-        print (self.token.key)
-
+        self.profile = Profile(user=self.user, instaAccount="insta").save()
 
     def api_Authentication(self):
-        print ("api_Authentication")
-        print (self.token.key)
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
-  
 #class ApiCreateEventTest(APITestCase):
 
     def test_a_for_create_event(self):
-        
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
         print("test_for_Create_event")
         print(self.token.key)
-        end_point ="/api/eventos/create_event/"
+        end_point="/api/eventos/create_event/"
         body = {
             "status" : "O",
             "stock" :99,
-            "title": "tituloapitest",
+            "title":"tituloapitest",
             "type": "A"
         }
         response = self.client.post(end_point, body)
         print(response.data)
-        #print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 #class ApiGetEvents(APITestCase):
-    def test_b_for_get_events(self):
-        
-
-
-        
+    def test_b_for_get_events(self):  
         #self.test_for_create_event(self)
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
         print("test_for_get_event")
         print(self.token.key)
-        self.event = Event(eventOwner=self.user, eventType="A", title="title", status = "O").save(),
+        self.event = Event(eventOwner=self.user, eventType="A", title="title", status="O").save()
 
         end_point = "/api/eventos/all_events/"
 
         response = self.client.get(end_point)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
 
     def test_c_for_watch_event(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
         print("test_watch_event")
-        self.event = Event(eventOwner=self.user, eventType="A", title="title to watch", status = "O").save(),
+        self.event = Event(eventOwner=self.user, eventType="A", title="title to watch", status="O").save()
 
         end_point = "/api/eventos/all_events/"
         response = self.client.get(end_point)
@@ -89,7 +71,7 @@ class ApiEvenCreateTests(APITestCase):
 
         end_point = "/api/accounts/eventWatch"
         body = {
-             'event_pk' : 3,   
+             'event_pk' : 3,
              'notToken' : '1'
          }
         response = self.client.post(end_point, body)
@@ -102,7 +84,7 @@ class ApiEvenCreateTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
         print("test_for_add_user_to_event")
         print(self.token.key)
-        self.event = Event(eventOwner=self.user, eventType="A", title="title add", status = "O").save(),
+        self.event = Event(eventOwner=self.user, eventType="A", title="title add", status="O").save()
         end_point = "/api/eventos/all_events/"
 
         response = self.client.get(end_point)
@@ -110,7 +92,7 @@ class ApiEvenCreateTests(APITestCase):
         pk = Event.objects.get(eventOwner=self.user).pk
         end_point = "/api/eventos/adduser"
         body = {
-            "pk" : pk,   
+            "pk" : pk,
             "notificationToken" : "1"
         }
         response = self.client.post(end_point, body)
@@ -132,11 +114,13 @@ class ApiEvenCreateTests(APITestCase):
         post.status = "W"
         post.save()
         pk = Event.objects.get(eventOwner=self.user).pk
-        
+        print(pk)
         end_point = "/api/eventos/finalize_event/"
         body = {
-             'pk' : pk,   
+             'pk' : pk,
              'data4Company' : '11110144123'
          }
         response = self.client.post(end_point, body)
+        post = Post.objects.get(person=self.user)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(post.status, "F")
