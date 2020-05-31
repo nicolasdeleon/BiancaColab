eab5ffd12516a69f5bb10dff76bb83212a527a3d
damian-bit <51974962+damian-bit@users.noreply.github.com>
2020-05-31 19:53:47 -0300

    Update settings.py

--- backBone_Bianca/settings.py
+++ backBone_Bianca/settings.py
@@ -240,3 +240,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+# AWS_S3_REGION_NAME = 's3v4'
+
